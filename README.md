d9756856621d0d227c8970c80eac5e99bde0e089
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-04-14 12:03:27 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+Project 	- CMSGears (http://www.cmsgears.org)
+
+Plugin  	- Webmaster
+
+License 	- GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+
+Description - Webmaster plugin for CMSGears provide options to configure webmasters.
+
+Highlighted Features
+=========================================
+1. It provide options to configure webmasters from admin.
+
+Notes: The detailed description for each feature can be found at http://www.cmsgears.org.
---
+++ migrations/m180414_061257_webmaster.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+// CMG Imports
+use cmsgears\core\common\config\CoreGlobal;
+
+use cmsgears\core\common\base\Migration;
+
+use cmsgears\core\common\models\entities\Site;
+use cmsgears\core\common\models\entities\User;
+use cmsgears\core\common\models\resources\Form;
+use cmsgears\core\common\models\resources\FormField;
+
+use cmsgears\core\common\utilities\DateUtil;
+
+/**
+ * The webmaster migration inserts the base data required for webmasters.
+ *
+ * @since 1.0.0
+ */
+class m180414_061257_webmaster extends Migration {
+
+	// Public Variables
+
+	// Private Variables
+
+	private $prefix;
+
+	private $site;
+	private $master;
+
+	public function init() {
+
+		// Table prefix
+		$this->prefix	= Yii::$app->migration->cmgPrefix;
+
+		$this->site		= Site::findBySlug( CoreGlobal::SITE_MAIN );
+		$this->master	= User::findByUsername( Yii::$app->migration->getSiteMaster() );
+
+		Yii::$app->core->setSite( $this->site );
+	}
+
+	public function up() {
+
+		// Create various config
+		$this->insertFileConfig();
+
+		// Init default config
+		$this->insertDefaultConfig();
+	}
+
+	private function insertFileConfig() {
+
+		$this->insert( $this->prefix . 'core_form', [
+			'siteId' => $this->site->id,
+			'createdBy' => $this->master->id, 'modifiedBy' => $this->master->id,
+			'name' => 'Config Webmaster', 'slug' => 'config-webmaster',
+			'type' => CoreGlobal::TYPE_SYSTEM,
+			'description' => 'Webmaster configuration form.',
+			'success' => 'All configurations saved successfully.',
+			'captcha' => false,
+			'visibility' => Form::VISIBILITY_PROTECTED,
+			'status' => Form::STATUS_ACTIVE, 'userMail' => false,'adminMail' => false,
+			'createdAt' => DateUtil::getDateTime(),
+			'modifiedAt' => DateUtil::getDateTime()
+		]);
+
+		$config	= Form::findBySlugType( 'config-webmaster', CoreGlobal::TYPE_SYSTEM );
+
+		$columns = [ 'formId', 'name', 'label', 'type', 'compress', 'meta', 'active', 'validators', 'order', 'icon', 'htmlOptions' ];
+
+		$fields	= [
+			[ $config->id, 'active', 'Active', FormField::TYPE_TOGGLE, false, true, true, 'required', 0, NULL, '{"title":"Active"}' ]
+		];
+
+		$this->batchInsert( $this->prefix . 'core_form_field', $columns, $fields );
+	}
+
+	private function insertDefaultConfig() {
+
+		$columns = [ 'modelId', 'name', 'label', 'type', 'active', 'valueType', 'value', 'data' ];
+
+		$metas	= [
+			[ $this->site->id, 'active', 'Active', 'webmaster', 1, 'flag', '1', NULL ]
+		];
+
+		$this->batchInsert( $this->prefix . 'core_site_meta', $columns, $metas );
+	}
+
+	public function down() {
+
+		echo "m180414_061257_webmaster will be deleted with m160621_014408_core.\n";
+
+		return true;
+	}
+
+}
